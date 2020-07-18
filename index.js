e018e5ab7a6cf67028b1d58acf3f946416eb5b10
takigavin <gavinhorner0@gmail.com>
2020-07-18 10:58:04 -0500

    Setup project and added basic text functionality

---
+++ index.js
@@ -0,0 +1,48 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+// require the config.json file
+const { prefix, token } = require('./main/config.json');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// login to Discord with your app's token
+client.login(token);
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	}
+	else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	else if (command === 'dateCreated') {
+		message.channel.send(`Server created on: ${message.guild.createdAt}`);
+	}
+});
+
+function getUserFromMention(mention) {
+	if (!mention) return;
+
+	if (mention.startsWith('<@') && mention.endsWith('>')) {
+		mention = mention.slice(2, -1);
+
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+
+		return client.users.cache.get(mention);
+	}
+}
